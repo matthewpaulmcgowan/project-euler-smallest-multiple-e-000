3b993a477f5655548f1eed7b5966992432ef701a
matthewpaulmcgowan <matthewpaulmcgowan@gmail.com>
2015-11-23 01:15:42 +0000

    Done.

--- lib/oo_smallest_multiple.rb
+++ lib/oo_smallest_multiple.rb
@@ -1,1 +1,26 @@
 # Implement your object-oriented solution here!
+class SmallestMultiple
+  def initialize input
+    @input=input
+  end
+  def input
+    @input
+  end
+  def lcm
+  a=[]
+   b=@input
+    while b>0
+    a.push(b)
+      b=b-1
+    end
+    number=@input
+  until number>100000
+      a.all? do |i| number%i==0
+      return number
+    end
+  number=number+1
+end
+
+
+  end
+end
--- lib/smallest_multiple.rb
+++ lib/smallest_multiple.rb
@@ -1,1 +1,21 @@
 # Implement your procedural solution here!
+def smallest_multiple(input)
+a=[]
+  b=input
+  while b>0
+    a.push(b)
+    b=b-1
+  end
+
+count=1
+  d=[0]
+until d.length==2
+a.all? do |i|
+  if count%i==0
+    d.unshift(count)
+  end
+  count=count+1
+end
+end
+return d[0]
+  end
